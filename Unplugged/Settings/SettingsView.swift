3bb6fbc99f955a85c0fa4c30a5815c58ec3d8121
Jade Keegan <97476936+jadekeegan@users.noreply.github.com>
2024-11-02 12:16:24 -0400

    Implement settings UI

--- Unplugged/Settings/SettingsView.swift
+++ Unplugged/Settings/SettingsView.swift
@@ -8,10 +8,33 @@
 import SwiftUI
 
 struct SettingsView: View {
+    @State private var isConnectAccountExpanded: Bool = true;
+    @State private var isInstagramConnected: Bool = false;
+    @State private var isFacebookConnected: Bool = false;
+    @State private var showLikes: Bool = false;
+    
     var body: some View {
         NavigationStack {
             List {
-                
+                DisclosureGroup(isExpanded: $isConnectAccountExpanded) {
+                    HStack() {
+                        Text("Instagram")
+                        Spacer()
+                        Button(action: {}) {
+                            !isInstagramConnected ? Text("Connect") : Text("Disconnect")
+                        }
+                    }
+                    HStack() {
+                        Text("Facebook")
+                        Spacer()
+                        Button(action: {}) {
+                            !isFacebookConnected ? Text("Connect") : Text("Disconnect")
+                        }
+                    }
+                } label: {
+                    Label("Connect Accounts", systemImage: "person.crop.square")
+                }
+                Toggle(isOn: $showLikes, label: { Label("Display Likes", systemImage: "heart") })
             }
             .navigationTitle("Settings")
         }
