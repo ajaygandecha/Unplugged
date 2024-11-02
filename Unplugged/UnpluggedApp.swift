95ad60c3889e9d1de9eab703568731dd3402cfd6
noah <90880131+noahsmiths@users.noreply.github.com>
2024-11-02 11:32:36 -0400

    Initial Commit

---
+++ Unplugged/UnpluggedApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnpluggedApp.swift
+//  Unplugged
+//
+//  Created by Noah Smith on 11/2/24.
+//
+
+import SwiftUI
+
+@main
+struct UnpluggedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
